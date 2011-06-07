6d0ef4c35ec029a2d2bca59b447325f6e68ad0b5
Renaud Delbru <renaud.delbru@deri.org>
2011-06-07 14:46:48 -0700

    First pass over siren-solr example

--- siren-solr/example/query.sh
+++ siren-solr/example/query.sh
@@ -15,7 +15,8 @@
 # limitations under the License.
 
 echo "Query: * <name> 'john AND gartner'"
-curl "http://localhost:8080/siren/select?indent=on&q=*+%3Cname%3E+%27john+AND+gartner%27&start=0&rows=10&fl=*%2Cscore&qt=ntriple&wt=standard" -H 'Content-type:text/xml; charset=utf-8'
+curl "http://localhost:8080/siren/select?indent=on&nq=*+%3Cname%3E+%27john+AND+gartner%27&start=0&rows=10&fl=*%2Cscore&qt=siren&wt=standard" -H 'Content-type:text/xml; charset=utf-8'
 
 echo "Query: * <workplace> <galway> AND * <type> <student> AND * <age> \"26\""
-curl "http://localhost:8080/siren/select?indent=on&q=*+%3Cworkplace%3E+%3Cgalway%3E+AND+*+%3Ctype%3E+%3Cstudent%3E+AND+*+%3Cage%3E+%2226%22&start=0&rows=10&fl=*%2Cscore&qt=ntriple&wt=standard" -H 'Content-type:text/xml; charset=utf-8'
+curl "http://localhost:8080/siren/select?indent=on&nq=*+%3Cworkplace%3E+%3Cgalway%3E+AND+*+%3Ctype%3E+%3Cstudent%3E+AND+*+%3Cage%3E+%2226%22&start=0&rows=10&fl=*%2Cscore&qt=siren&wt=standard" -H 'Content-type:text/xml; charset=utf-8'
+

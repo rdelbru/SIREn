8a6735ff9e46e5b44ce2c609f39e5c3f069b113b
scampi <stephane.campinas@deri.org>
2011-09-07 10:55:13 +0100

    Change the license to GPL

--- siren-qparser/src/test/java/org/sindice/siren/qparser/analysis/NTripleAnalyzerTestHelper.java
+++ siren-qparser/src/test/java/org/sindice/siren/qparser/analysis/NTripleAnalyzerTestHelper.java
@@ -1,10 +1,25 @@
 /**
- * 
- * ------------------------------------------------------------
+ * Copyright (c) 2009-2011 Sindice Limited. All Rights Reserved.
  *
+ * Project and contact information: http://www.siren.sindice.com/
+ *
+ * This file is part of the SIREn project.
+ *
+ * SIREn is a free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SIREn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SIREn. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
  * @project solr-plugins
- *
- * Copyright (C) 2007,
  * @author Renaud Delbru [ 3 mars 08 ]
  * @link http://renaud.delbru.fr/
  * All rights reserved.

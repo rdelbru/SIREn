9c7826c9f05a83c15e58d85c254da7955475576c
scampi <stephane.campinas@deri.org>
2011-09-12 18:19:21 +0100

    modified the solr example changed the keyword query parser exception to avoid the "bundle" exception on tomcat

--- siren-qparser/src/main/java/org/sindice/siren/qparser/keyword/KeywordQParserImpl.java
+++ siren-qparser/src/main/java/org/sindice/siren/qparser/keyword/KeywordQParserImpl.java
@@ -66,7 +66,7 @@ public class KeywordQParserImpl {
       }
     }
     catch (final QueryNodeException e) {
-      throw new ParseException(e.getMessage());
+      throw new ParseException("Invalid keyword query");
     }
   }
 
--- siren-solr/example/query.sh
+++ siren-solr/example/query.sh
@@ -14,9 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo "Query: galway"
+curl "http://localhost:8080/siren/select?indent=on&q=galway&start=0&rows=10&fl=*%2Cscore&qt=siren&wt=standard"
+
 echo "Query: * <name> 'john AND gartner'"
 curl "http://localhost:8080/siren/select?indent=on&nq=*+%3Cname%3E+%27john+AND+gartner%27&start=0&rows=10&fl=*%2Cscore&qt=siren&wt=standard" -H 'Content-type:text/xml; charset=utf-8'
 
 echo "Query: * <workplace> <galway> AND * <type> <student> AND * <age> \"26\""
 curl "http://localhost:8080/siren/select?indent=on&nq=*+%3Cworkplace%3E+%3Cgalway%3E+AND+*+%3Ctype%3E+%3Cstudent%3E+AND+*+%3Cage%3E+%2226%22&start=0&rows=10&fl=*%2Cscore&qt=siren&wt=standard" -H 'Content-type:text/xml; charset=utf-8'
 
+echo "Query: galway AND * <type> <student>"
+curl "http://localhost:8080/siren/select?indent=on&version=2.2&q=galway&nq=*+%3Ctype%3E+%3Cstudent%3E&fq=&start=0&rows=10&fl=*%2Cscore&qt=siren&wt=standard"

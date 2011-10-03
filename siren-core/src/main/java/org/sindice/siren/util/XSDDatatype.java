bf9817b07b4b825598af497d6d57aaf903b5d51e
Renaud Delbru <renaud.delbru@deri.org>
2011-10-03 17:50:05 +0100

    Added list of xsd datatype #20

---
+++ siren-core/src/main/java/org/sindice/siren/util/XSDDatatype.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2009-2011 Sindice Limited. All Rights Reserved.
+ *
+ * Project and contact information: http://www.siren.sindice.com/
+ *
+ * This file is part of the SIREn project.
+ *
+ * SIREn is a free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * SIREn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with SIREn. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @project siren-core
+ * @author Renaud Delbru [ 3 Oct 2011 ]
+ * @link http://renaud.delbru.fr/
+ */
+package org.sindice.siren.util;
+
+/**
+ * List of datatypes in the xsd: namespace
+ */
+public class XSDDatatype {
+
+  /** The xsd namespace */
+  public static final String XSD = "http://www.w3.org/2001/XMLSchema#";
+
+  /** Datatype representing xsd:float */
+  public static final String XSD_FLOAT = XSD + "float";
+
+  /** Datatype representing xsd:double */
+  public static final String XSD_DOUBLE = XSD + "double";
+
+  /** Datatype representing xsd:int */
+  public static final String XSD_INT = XSD + "int";
+
+  /** Datatype representing xsd:long */
+  public static final String XSD_LONG = XSD + "long";
+
+  /** Datatype representing xsd:short */
+  public static final String XSD_SHORT = XSD + "short";
+
+  /** Datatype representing xsd:byte */
+  public static final String XSD_BYTE = XSD + "byte";
+
+  /** Datatype representing xsd:unsignedByte */
+  public static final String XSD_UNSIGNED_BYTE = XSD + "unsignedByte";
+
+  /** Datatype representing xsd:unsignedShort */
+  public static final String XSD_UNSIGNED_SHORT = XSD + "unsignedShort";
+
+  /** Datatype representing xsd:unsignedInt */
+  public static final String XSD_UNISGNED_INT = XSD + "unsignedInt";
+
+  /** Datatype representing xsd:unsignedLong */
+  public static final String XSD_UNSIGNED_LONG = XSD + "unsignedLong";
+
+  /** Datatype representing xsd:decimal */
+  public static final String XSD_DECIMAL = XSD + "decimal";
+
+  /** Datatype representing xsd:integer */
+  public static final String XSD_INTEGER = XSD + "integer";
+
+  /** Datatype representing xsd:nonPositiveInteger */
+  public static final String XSD_NON_POSITIVE_INTEGER = XSD + "nonPositiveInteger";
+
+  /** Datatype representing xsd:nonNegativeInteger */
+  public static final String XSD_NON_NEGATIVE_INTEGER = XSD + "nonNegativeInteger";
+
+  /** Datatype representing xsd:positiveInteger */
+  public static final String XSD_POSITIVE_INTEGER = XSD + "positiveInteger";
+
+  /** Datatype representing xsd:negativeInteger */
+  public static final String XSD_NEGATIVE_INTEGER = XSD + "negativeInteger";
+
+  /** Datatype representing xsd:boolean */
+  public static final String XSD_BOOLEAN = XSD + "boolean";
+
+  /** Datatype representing xsd:string */
+  public static final String XSD_STRING = XSD + "string";
+
+  /** Datatype representing xsd:normalizedString */
+  public static final String XSD_NORMALISED_STRING = XSD + "normalizedString";
+
+  /** Datatype representing xsd:anyURI */
+  public static final String XSD_ANY_URI = XSD + "anyURI";
+
+  /** Datatype representing xsd:token */
+  public static final String XSD_TOKEN = XSD + "token";
+
+  /** Datatype representing xsd:Name */
+  public static final String XSD_NAME = XSD + "Name";
+
+  /** Datatype representing xsd:QName */
+  public static final String XSD_QNAME = XSD + "QName";
+
+  /** Datatype representing xsd:language */
+  public static final String XSD_LANGUAGE = XSD + "language";
+
+  /** Datatype representing xsd:NMTOKEN */
+  public static final String XSD_NMTOKEN = XSD + "NMTOKEN";
+
+  /** Datatype representing xsd:ENTITY */
+  public static final String XSD_ENTITY = XSD + "ENTITY";
+
+  /** Datatype representing xsd:ID */
+  public static final String XSD_ID = XSD + "ID";
+
+  /** Datatype representing xsd:NCName */
+  public static final String XSD_NCName = XSD + "NCName";
+
+  /** Datatype representing xsd:IDREF */
+  public static final String XSD_IDREF = XSD + "IDREF";
+
+  /** Datatype representing xsd:NOTATION */
+  public static final String XSD_NOTATION = XSD + "NOTATION";
+
+  /** Datatype representing xsd:hexBinary */
+  public static final String XSD_HEX_BINARY = XSD + "hexBinary";
+
+  /** Datatype representing xsd:base64Binary */
+  public static final String XSD_BASE64_BINARY = XSD + "base64Binary";
+
+  /** Datatype representing xsd:date */
+  public static final String XSD_DATE = XSD + "date";
+
+  /** Datatype representing xsd:time */
+  public static final String XSD_TIME = XSD + "time";
+
+  /** Datatype representing xsd:dateTime */
+  public static final String XSD_DATE_TIME = XSD + "dateTime";
+
+  /** Datatype representing xsd:duration */
+  public static final String XSD_DURATION = XSD + "duration";
+
+  /** Datatype representing xsd:gDay */
+  public static final String XSD_G_DAY = XSD + "gDay";
+
+  /** Datatype representing xsd:gMonth */
+  public static final String XSD_G_MONTH = XSD + "gMonth";
+
+  /** Datatype representing xsd:gYear */
+  public static final String XSD_G_YEAR = XSD + "gYear";
+
+  /** Datatype representing xsd:gYearMonth */
+  public static final String XSD_G_YEAR_MONTH = XSD + "gYearMonth";
+
+  /** Datatype representing xsd:gMonthDay */
+  public static final String XSD_G_MONTH_DAY = XSD + "gMonthDay";
+
+}

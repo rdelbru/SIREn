b79f91837f394062702cdcc9ff5440eed7aa14f2
Renaud Delbru <renaud.delbru@deri.org>
2011-10-05 11:12:34 +0100

    cleaned some code. updated caliper bench. #20

---
+++ siren-core/src/test/java/org/sindice/siren/analysis/bench/PlainTupleAnalyzer.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2009-2011 Sindice Limited. All Rights Reserved.
+ *
+ * Project and contact information: http://www.siren.sindice.com/
+ *
+ * This file is part of the SIREn project.
+ *
+ * SIREn is a free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * SIREn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with SIREn. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @project siren
+ * @author Renaud Delbru [ 8 Dec 2009 ]
+ * @link http://renaud.delbru.fr/
+ * @copyright Copyright (C) 2009 by Renaud Delbru, All rights reserved.
+ */
+package org.sindice.siren.analysis.bench;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.sindice.siren.analysis.TupleTokenizer;
+import org.sindice.siren.analysis.filter.TokenTypeFilter;
+
+/**
+ * TupleAnalyzer that only defines the {@link TupleTokenizer} wihtout any
+ * filters.
+ */
+public class PlainTupleAnalyzer extends Analyzer {
+
+  @Override
+  public final TokenStream tokenStream(final String fieldName, final Reader reader) {
+    final TupleTokenizer stream = new TupleTokenizer(reader, Integer.MAX_VALUE);
+    final TokenStream result = new TokenTypeFilter(stream, new int[] {TupleTokenizer.BNODE,
+                                                                TupleTokenizer.DOT});
+    return result;
+  }
+
+  @Override
+  public final TokenStream reusableTokenStream(final String fieldName, final Reader reader) throws IOException {
+    SavedStreams streams = (SavedStreams) this.getPreviousTokenStream();
+    if (streams == null) {
+      streams = new SavedStreams();
+      this.setPreviousTokenStream(streams);
+      streams.tokenStream = new TupleTokenizer(reader, Integer.MAX_VALUE);
+      streams.filteredTokenStream = new TokenTypeFilter(streams.tokenStream,
+        new int[] {TupleTokenizer.BNODE, TupleTokenizer.DOT});
+    } else {
+      streams.tokenStream.reset(reader);
+    }
+    return streams.filteredTokenStream;
+  }
+
+  private static final class SavedStreams {
+    TupleTokenizer tokenStream;
+    TokenStream filteredTokenStream;
+  }
+
+}
